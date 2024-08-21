8be3168a7df6b4eb8f8969d07efb96e6a7d31b17
KofiGhana <abkarikari23@yahoo.co.uk>
2024-08-21 09:09:47 +0000

    React Intro

---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+// import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications/Notifications';
+
+//cannot redeclare block-scope variable root
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root-notifications = ReactDOM.createRoot(document.getElementById('root-notifications'))
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
